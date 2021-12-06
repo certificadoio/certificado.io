8ef4bb55b7b5b64c98b13e30ed6ce3bb2bd27874
Luã Álvaro <luan.alc@hotmail.com>
2021-12-06 09:19:27 -0300

    Adição do chat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import "@fontsource/inter"
 
@@ -13,6 +14,12 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>Certificado.io</title>
+        <script dangerouslySetInnerHTML={{
+          __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="b42f1606-7b12-4d0a-b07e-ecbe710e8c7c";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`
+        }}></script>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
